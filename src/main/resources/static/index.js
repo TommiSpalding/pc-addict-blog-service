5654fb26f273bd86047ac4a5fcd8b0005b1b8edd
TommiSpalding <thomas.spalding@cs.tamk.fi>
2018-04-05 00:14:04 +0300

    Implemented article searching

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -7,7 +7,7 @@ function Blogpost(properties) {
             <a href="#" class="btn btn-primary">Read More &rarr;</a>
         </div>
         <div className="card-footer text-muted">
-            {new Date(Number(properties.timePosted)*1000).toDateString()} by <a href="#">{properties.authorName}</a>
+            {new Date(Number(properties.timePosted)*1000).toDateString()} by <a href="#" value={properties.authorName} onClick={() => blogpostsByAuthorName(properties.authorName) }>{properties.authorName}</a>
         </div>
     </div>;
 }
@@ -24,10 +24,30 @@ function ManyBlogposts(properties) {
     return <div>{output}</div>
 }
 
-function createBlogpostTable() {
+function allBlogposts() {
     fetch('http://localhost:8080/blogposts').then((response) => response.json()).then((arr) => {
         ReactDOM.render(<ManyBlogposts array={arr}/>,document.getElementById("blogposts"));
     });
 }
 
-createBlogpostTable();
+function blogpostsByAuthorName(authorName) {
+    fetch(`http://localhost:8080/blogposts/searchAuthor?q=${authorName}`).then((response) => response.json()).then((arr) => {
+        ReactDOM.render(<ManyBlogposts array={arr}/>,document.getElementById("blogposts"));
+    });
+}
+
+function blogpostsByTitle(titleName) {
+    console.log('paska paska paska')
+    fetch(`http://localhost:8080/blogposts/search?q=${titleName}`).then((response) => response.json()).then((arr) => {
+        ReactDOM.render(<ManyBlogposts array={arr}/>,document.getElementById("blogposts"));
+    });
+}
+
+function searchTitle() {
+    console.log('vittu vittu vittu')
+    let title = document.getElementById('searchInput').value;
+    blogpostsByTitle(title);
+}
+
+allBlogposts();
+
